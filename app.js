691fd163482c967c9b2bfda8935442fefc86f2c0
Igor Suhachov <42006386+IgorSuhachov@users.noreply.github.com>
2023-06-05 05:56:19 +0300

    .

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(router);
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
 });
