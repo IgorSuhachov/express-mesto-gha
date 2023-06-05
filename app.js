c54e6ede770241bc55130bdd2b525b0340c418da
Igor Suhachov <42006386+IgorSuhachov@users.noreply.github.com>
2023-06-05 06:13:37 +0300

    )

--- app.js
+++ app.js
@@ -11,9 +11,11 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(router);
+app.use(errors());
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
+  next();
 });
 
 mongoose
@@ -24,7 +26,6 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-app.use(errors());
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на ${PORT} порту`);
