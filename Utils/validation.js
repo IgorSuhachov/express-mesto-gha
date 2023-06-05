e62327b484c1766c599e832b9508fbbaf68637c9
Igor Suhachov <42006386+IgorSuhachov@users.noreply.github.com>
2023-06-05 15:11:55 +0300

    убейте меня)

--- Utils/validation.js
+++ Utils/validation.js
@@ -39,7 +39,7 @@ const createCardValidation = celebrate({
 });
 
 const getUserIdValidation = celebrate({
-  body: Joi.object().keys({
+  params: Joi.object().keys({
     id: Joi.string().hex().length(24).required(),
   }),
 });
