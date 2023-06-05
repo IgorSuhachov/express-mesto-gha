195e27b72d074f16b0c8257fd6a212f04f7279bd
Igor Suhachov <42006386+IgorSuhachov@users.noreply.github.com>
2023-06-05 04:38:05 +0300

    тесты json

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(router);
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
+  next();
+});
 
 mongoose
   .connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true, useUnifiedTopology: true, family: 4 })
